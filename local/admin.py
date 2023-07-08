26dc914854a1f9454e6ec9f5c133c316d21cb0be
FcaHJ <127349236+FcaHJ@users.noreply.github.com>
2023-07-08 05:16:21 -0400

    Se crean los models

--- local/admin.py
+++ local/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Rol,User,Category,Product,Boleta,Detalle_Boleta
 
 # Register your models here.
+admin.site.register(Rol)
+admin.site.register(User)
+admin.site.register(Category)
+admin.site.register(Product)
+admin.site.register(Boleta)
+admin.site.register(Detalle_Boleta)
--- local/models.py
+++ local/models.py
@@ -1,8 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class Rol(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
 
 class User(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField()
     password = models.CharField(max_length=50)
+    rol = models.ForeignKey(Rol, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField(max_length=200)
+    price = models.IntegerField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
+class Boleta(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField(max_length=200)
+    price = models.IntegerField()
+    
+    def __str__(self):
+        return self.name
+
+class Detalle_Boleta(models.Model):
+    name = models.CharField(max_length=70)
+    description = models.TextField(max_length=200)
+    price = models.IntegerField()
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+    
