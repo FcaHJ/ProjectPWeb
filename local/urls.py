8fd29ad7971c5e4aed31414814bfc1c6ccb62a8a
FcaHJ <127349236+FcaHJ@users.noreply.github.com>
2023-07-09 00:55:19 -0400

    Se crea el registro

--- local/urls.py
+++ local/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path("nosotros/", views.nosotros, name="nosotros"),
     path("terminos/", views.terminos, name="terminos"),
     path("perfil/", views.perfil, name="perfil"),
+    path("registro/", views.registro, name="registro"),
+    path("inicio_sesion/", views.sesion, name="inicio_sesion"),
+    path("logout/", views.cerrar_sesion, name="cerrar_sesion"),
 ]
--- local/views.py
+++ local/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.contrib.auth import login,logout
 
 # Create your views here.
 def home(request):
@@ -14,3 +16,31 @@ def terminos(request):
 
 def perfil(request):
     return render(request, "local/profile.html")
+
+def registro(request):
+    if request.method == 'GET':
+        return render(request, "local/registro.html",{
+            'form' : UserCreationForm
+    })  
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                user = User.objects.create_user(
+                    
+                )
+            except:
+                return render(request, "local/registro.html",{
+            'form' : UserCreationForm,
+            'error' : 'El usuario ya existe'
+            })  
+        return render(request, "local/registro.html",{
+            'form' : UserCreationForm,
+            'error' : 'Las contraseñas no coinciden'
+    })  
+
+def sesion(request):
+    return render(request, "local/inicio_sesion.html")
+
+def cerrar_sesion(request):
+    logout(request)
+    return redirect('index')
