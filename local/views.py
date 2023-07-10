eb7c55b53c4e24b6f764381024c4caf2d4ec6c71
FcaHJ <127349236+FcaHJ@users.noreply.github.com>
2023-07-10 09:31:13 -0400

    se agregan vistas

--- local/views.py
+++ local/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import  AuthenticationForm
+from django.contrib.auth import login,logout,authenticate
+from .forms import CustomUserForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login,logout
+from .models import Product
+from local.carrito import Carrito
 
 # Create your views here.
 def home(request):
@@ -14,33 +17,85 @@ def nosotros(request):
 def terminos(request):
     return render(request, "local/terminos.html")
 
+def perfil_log(request):
+    return render(request, "local/profile_log.html")
+
 def perfil(request):
     return render(request, "local/profile.html")
 
+def productos(request):
+    productos = Product.objects.all()
+    data = {
+        'productos' : productos
+    }
+    return render(request, "local/productos.html",data)
+
 def registro(request):
     if request.method == 'GET':
-        return render(request, "local/registro.html",{
-            'form' : UserCreationForm
-    })  
+        return render(request, 'local/registro.html',{
+            'form' : CustomUserForm
+        })
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
                 user = User.objects.create_user(
-                    
-                )
-            except:
-                return render(request, "local/registro.html",{
-            'form' : UserCreationForm,
-            'error' : 'El usuario ya existe'
-            })  
-        return render(request, "local/registro.html",{
-            'form' : UserCreationForm,
-            'error' : 'Las contraseñas no coinciden'
-    })  
+                    username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('home')
+            except: 
+                return render(request, 'local/registro.html',{
+                    'form' : CustomUserForm,
+                    "error" : 'El nombre de usuario ya existe'
+                })
+        return render(request, 'local/registro.html',{
+                    'form' : CustomUserForm,
+                    "error" : 'Las contraseñas no coinciden'
+                })
+        
+    return render(request, "local/registro.html")  
 
-def sesion(request):
-    return render(request, "local/inicio_sesion.html")
+def inicio_sesion(request):
+    if request.method == 'GET':
+        return render(request, "local/inicio_sesion.html",{
+            'form' : AuthenticationForm
+        })
+    else:
+        user = authenticate(
+            request,username=request.POST['username'], 
+            password=request.POST['password'])
+        if user is None:
+            return render(request, 'inicio_sesion',{
+                'form' : AuthenticationForm,
+                'error' : 'Usuario o contraseña incorrecta'
+            })
+        else:
+            login(request,user)
+            return redirect(to='home')
 
 def cerrar_sesion(request):
     logout(request)
-    return redirect('index')
+    return redirect('home')
+
+def agregar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Product.objects.get(id=producto_id)
+    carrito.agregar(producto)
+    return redirect("productos")
+
+def eliminar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Product.objects.get(id=producto_id)
+    carrito.eliminar(producto)
+    return redirect("productos")
+
+def restar_producto(request, producto_id):
+    carrito = Carrito(request)
+    producto = Product.objects.get(id=producto_id)
+    carrito.restar(producto)
+    return redirect("productos")
+
+def limpiar_producto(request):
+    carrito = Carrito(request)
+    carrito.limpiar()
+    return redirect("productos")
