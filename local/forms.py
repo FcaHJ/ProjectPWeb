9ce7fd4bffeea7e34c1aa9a19c232137d4cbfadb
FcaHJ <127349236+FcaHJ@users.noreply.github.com>
2023-07-10 09:31:56 -0400

    se crea formulario CustomUserForm

--- local/forms.py
+++ local/forms.py
@@ -1,2 +1,13 @@
 from django import forms
+from django.forms import ModelForm
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
+
+class CustomUserForm(UserCreationForm):
+    
+    class Meta:
+        model = User
+        fields = ('username','email','password1','password2')
+        
+        
