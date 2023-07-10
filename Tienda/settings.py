2e2785e944b61b09a36215cb226037ad12841e13
FcaHJ <127349236+FcaHJ@users.noreply.github.com>
2023-07-10 02:53:28 -0400

    se importo humanize

--- Tienda/settings.py
+++ Tienda/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'local'
+    'django.contrib.humanize',
+    'local',
 ]
 
 MIDDLEWARE = [
